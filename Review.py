4ae7d9f28f2f007b5935a89850c037c694439d9a
Zach Barker <zachbarker64@gmail.com>
2020-07-01 13:08:57 -0400

    Initial commit

---
+++ Review.py
@@ -0,0 +1,40 @@
+#Functions
+'''
+def example(x, y, z):   in this form, x, y, and z are called parameters.
+    result = (x + y) * z
+    print(result)
+
+def example(1, 2, 3) in this form, 1, 2, and 3 are called arguments]
+                     this is referred to as 'calling' a function.
+
+    Output for example(1, 2, 3): 9
+-----------------------------------------------------------------------------------
+
+def start(x):
+     return x
+
+def add(y, z):
+    result = start(y) + z
+    return result
+
+def multiply(a, b, c):
+    result = add(a, b) * c
+    print(result)
+
+start(1): Return value: 1
+add(1, 2): Return value: 3
+multiply(1, 2, 3):  Output: 9
+
+'''
+def example(x, y, z):
+    pass
+
+
+
+
+#Arrays
+
+
+
+
+#Functions and Arrays
